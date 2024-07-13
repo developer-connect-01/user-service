91243bdf3df06463c7d61812f740e72fb1bf2136
ANUSRUTA DUTTA <anusruta.dutta@gmail.com>
2024-07-13 14:15:00 +0530

    Add DemoController

---
+++ src/main/java/com/developersconnect/userservice/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.developersconnect.userservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+public class DemoController {
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello from secured endpoint");
+    }
+}
